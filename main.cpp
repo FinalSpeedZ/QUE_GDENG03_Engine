5b56228a00c74c7eea1bb1e3c4eb34f9c9c44fd4
FinalSpeedZ <85392584+FinalSpeedZ@users.noreply.github.com>
2024-12-03 19:54:54 +0800

    Cloned Scene Editor
    From GROUP 2 Scene Editor

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "AppWindow.h"
+
+int main()
+{
+	AppWindow::initialize();
+	AppWindow* runningApp = (AppWindow*)AppWindow::getInstance();
+	//runningApp->initializeEngine();
+
+	while(runningApp->isRunning())
+	{
+		runningApp->broadcast();
+	}
+
+	return 0;
+}
